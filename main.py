19be7685870691bb579472c349b13891f2adb01b
CallaJun <maracondo@aol.com>
2015-01-24 15:18:53 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import webapp2
+import jinja2
+import logging
+import os
+import urllib2
+from bs4 import BeautifulSoup
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+    	template_values = {
+        }
+        template = jinja_environment.get_template('views/index.html')
+        self.response.out.write(template.render(template_values))
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
