ff38c33a24e48165a264920afaf0fd18cd058e58
CallaJun <maracondo@aol.com>
2015-01-24 19:08:57 -0500

    All Haffner dining hall information

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ jinja_environment = jinja2.Environment(loader=
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+
+    	#ERDMAN
         #The page
         erdpage = urllib2.urlopen('http://www.brynmawr.edu/dining/ErdmanWebMenu.html').read()
         erdsoup = BeautifulSoup(erdpage)
@@ -39,10 +41,40 @@ class MainHandler(webapp2.RequestHandler):
         for menu in erdmenus:
         	erdmenus[erdmenus.index(menu)] = menu.split(",")
 
+        #HAFFNER
+        #The page
+        hafpage = urllib2.urlopen('http://www.brynmawr.edu/dining/HaffnerWebMenu.html').read()
+        hafsoup = BeautifulSoup(hafpage)
+
+        #Dates!
+        hafdates = hafsoup.find_all('h3')
+        #Dates: remove tags
+        for date in hafdates:
+        	hafdates[hafdates.index(date)] = str(date).replace("<h3>","").replace("</h3>","")
+
+        #Meal names!
+        hafmeals = hafsoup.find_all('th')
+        #Meal names: remove tags
+        for meal in hafmeals:
+        	hafmeals[hafmeals.index(meal)] = str(meal).replace("<th scope=\"row\">","").replace("</th>","")
+
+        #Menus!
+        hafmenus = hafsoup.find_all('td')
+        #Menus: take away the tags
+        for menu in hafmenus:
+        	hafmenus[hafmenus.index(menu)] = str(menu).replace("<br/>",",").replace("<td>","").replace("</td>","").replace("<h6>","").replace("\n","").replace("\t","").replace("<p>","").replace("</p>","").replace(" ","")
+        	#insert spaces
+        #Menus: make it a list
+        for menu in hafmenus:
+        	hafmenus[hafmenus.index(menu)] = menu.split(",")
+
         template_values = {
         "erddates": erddates,
         "erdmeals": erdmeals,
-        "erdmenus": erdmenus
+        "erdmenus": erdmenus,
+        "hafdates": hafdates,
+        "hafmeals": hafmeals,
+        "hafmenus": hafmenus,
         }
 
         template = jinja_environment.get_template('views/index.html')
